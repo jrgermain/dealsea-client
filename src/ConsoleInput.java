6fb095e0a595ab42ee9d315f198518c4760808e5
CTMaiorino <32141103+CTMaiorino@users.noreply.github.com>
2019-11-10 18:26:24 -0500

    Created ConsoleInput.java class
    Reads in the users keyword search, and the frequency of the search.

---
+++ src/ConsoleInput.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ConsoleInput {
+
+    private int interval;
+    private String keyWord;
+    private Scanner io = new Scanner(System.in);
+
+    ConsoleInput(){
+        //Input
+        System.out.println("---Deal Search---");
+        System.out.println("Enter a Item you want to search");
+        keyWord = io.nextLine();
+        System.out.println("Refresh Frequency( min(s) )");
+        interval = io.nextInt();
+
+        //Output
+        System.out.println("All Set. You will be notified when we find a match for your search.");
+        System.out.println("Item(s) Searched:" + "\n" + "\t" + keyWord);
+        System.out.println("Frequency: " + interval +" min(s)");
+        io.close();
+    }
+
+}
