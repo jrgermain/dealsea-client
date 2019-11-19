d934af0b52c1f55863855c030e789ab69b08421b
Joseph Germain <37086292+jrgermain@users.noreply.github.com>
2019-11-19 17:31:31 -0500

    Get results from console input and put in beginning of main loop

--- src/DealseaClient.java
+++ src/DealseaClient.java
@@ -1,7 +1,17 @@
 public class DealseaClient {
 
-	public static void main(String[] args){
-
+	public static void main(String[] args) throws InterruptedException {
+		// Read console input
 		ConsoleInput search = new ConsoleInput();
+		int intervalMinutes = search.getInterval();
+		long intervalMillis = intervalMinutes * 60 * 1000;
+		String keyWord = search.getKeyWord();
+
+		while (true) {
+			long startTime = System.currentTimeMillis();
+			// TODO Parse page
+			long endTime = System.currentTimeMillis();
+			Thread.sleep(intervalMillis - (endTime - startTime));
+		}
 	}
 }
